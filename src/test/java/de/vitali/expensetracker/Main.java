85250c8e20d66fcd54e2c21807b35d95f2228f61
Vitali.B <baklitskiy88@gmail.com>
2026-08-20 19:41:57 +0200

    Initial commit

---
+++ src/test/java/de/vitali/expensetracker/Main.java
@@ -0,0 +1,45 @@
+package de.vitali.expensetracker;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            double balance = 0;
+        while (true) {
+            System.out.println("=== Expense Tracker===");
+            System.out.println("1. Add income");
+            System.out.println("2. Add expense");
+            System.out.println("3. Show transactions");
+            System.out.println("4. Show balance");
+            System.out.println("0. Exit");
+            System.out.println("Choose option:");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    double income = scanner.nextDouble();
+                    balance = balance + income;
+                    System.out.println("Income added: " + income);
+                    System.out.println("Current balance" + balance);
+                    break;
+                case 2:
+                    double expense = scanner.nextDouble();
+                    balance = balance - expense;
+                    System.out.println("Expense added: " + expense);
+                    System.out.println("Current balance " + balance);
+                    break;
+                case 3:
+                    System.out.println("Transaction");
+                    break;
+                case 4:
+                    System.out.println("Current balance" + balance);
+                    break;
+                case 0:
+                    System.out.println("Exit");
+                    return;
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+    }
+}
