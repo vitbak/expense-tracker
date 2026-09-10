3d1112b0154d992e1c43d0fee46cf4cafb7c546b
Vitali.B <baklitskiy88@gmail.com>
2026-09-10 06:56:46 +0200

    Add transaction  model

--- src/main/java/de/vitali/expensetracker/Main.java
+++ src/main/java/de/vitali/expensetracker/Main.java
@@ -1,11 +1,15 @@
 package de.vitali.expensetracker;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
             Scanner scanner = new Scanner(System.in);
             double balance = 0;
+
+        ArrayList<Transaction> transactions = new ArrayList<>();
         while (true) {
             System.out.println("=== Expense Tracker===");
             System.out.println("1. Add income");
@@ -20,19 +24,25 @@ public class Main {
                     double income = scanner.nextDouble();
                     balance = balance + income;
                     System.out.println("Income added: " + income);
-                    System.out.println("Current balance" + balance);
+                    System.out.println("Current balance: " + balance);
+                    transactions.add(new Transaction("Income", income));
                     break;
                 case 2:
                     double expense = scanner.nextDouble();
                     balance = balance - expense;
                     System.out.println("Expense added: " + expense);
-                    System.out.println("Current balance " + balance);
+                    System.out.println("Current balance: " + balance);
+                    transactions.add(new Transaction("Expense", expense));
                     break;
                 case 3:
-                    System.out.println("Transaction");
+                    System.out.println("=== Transactions ===");
+
+                    for (Transaction transaction : transactions) {
+                        System.out.println(transaction.type + ": " + transaction.amount);
+                    }
                     break;
                 case 4:
-                    System.out.println("Current balance" + balance);
+                    System.out.println("Current balance: " + balance);
                     break;
                 case 0:
                     System.out.println("Exit");
---
+++ src/main/java/de/vitali/expensetracker/Transaction.java
@@ -0,0 +1,10 @@
+package de.vitali.expensetracker;
+
+public class Transaction {
+    String type;
+    double amount;
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+}
